1a32463f5e22b8ec87699352eac1e0ed11a05174
wcruz0520 <william.cruz@soluciones-sap.com>
2025-09-03 18:40:55 -0500

    carga udo reembolso

---
+++ CargaReembolso/CargaReemForm.Designer.cs
@@ -0,0 +1,242 @@
+﻿
+namespace CargaReembolso
+{
+    partial class CargaReemForm
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLimpUDO = new System.Windows.Forms.Button();
+            this.btnProcesar = new System.Windows.Forms.Button();
+            this.btnSimular = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbDocs = new System.Windows.Forms.ComboBox();
+            this.cmbUDOs = new System.Windows.Forms.ComboBox();
+            this.btnCUDO = new System.Windows.Forms.Button();
+            this.btnCDOC = new System.Windows.Forms.Button();
+            this.gridUDO = new System.Windows.Forms.DataGridView();
+            this.gridDOC = new System.Windows.Forms.DataGridView();
+            this.btnLimpDoc = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.gridUDO)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridDOC)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLimpUDO
+            // 
+            this.btnLimpUDO.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpUDO.Location = new System.Drawing.Point(1285, 45);
+            this.btnLimpUDO.Name = "btnLimpUDO";
+            this.btnLimpUDO.Size = new System.Drawing.Size(137, 34);
+            this.btnLimpUDO.TabIndex = 0;
+            this.btnLimpUDO.Text = "LIMPIAR";
+            this.btnLimpUDO.UseVisualStyleBackColor = true;
+            // 
+            // btnProcesar
+            // 
+            this.btnProcesar.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProcesar.Location = new System.Drawing.Point(29, 801);
+            this.btnProcesar.Name = "btnProcesar";
+            this.btnProcesar.Size = new System.Drawing.Size(137, 34);
+            this.btnProcesar.TabIndex = 1;
+            this.btnProcesar.Text = "PROCESAR";
+            this.btnProcesar.UseVisualStyleBackColor = true;
+            // 
+            // btnSimular
+            // 
+            this.btnSimular.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSimular.Location = new System.Drawing.Point(205, 801);
+            this.btnSimular.Name = "btnSimular";
+            this.btnSimular.Size = new System.Drawing.Size(137, 34);
+            this.btnSimular.TabIndex = 2;
+            this.btnSimular.Text = "SIMULAR";
+            this.btnSimular.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 435);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Tipo de Documento";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Tipo de UDO";
+            // 
+            // cmbDocs
+            // 
+            this.cmbDocs.FormattingEnabled = true;
+            this.cmbDocs.Items.AddRange(new object[] {
+            "Factura deudor"});
+            this.cmbDocs.Location = new System.Drawing.Point(205, 428);
+            this.cmbDocs.Name = "cmbDocs";
+            this.cmbDocs.Size = new System.Drawing.Size(174, 24);
+            this.cmbDocs.TabIndex = 5;
+            // 
+            // cmbUDOs
+            // 
+            this.cmbUDOs.FormattingEnabled = true;
+            this.cmbUDOs.Items.AddRange(new object[] {
+            "Reembolso"});
+            this.cmbUDOs.Location = new System.Drawing.Point(205, 55);
+            this.cmbUDOs.Name = "cmbUDOs";
+            this.cmbUDOs.Size = new System.Drawing.Size(174, 24);
+            this.cmbUDOs.TabIndex = 6;
+            // 
+            // btnCUDO
+            // 
+            this.btnCUDO.Location = new System.Drawing.Point(437, 56);
+            this.btnCUDO.Name = "btnCUDO";
+            this.btnCUDO.Size = new System.Drawing.Size(75, 23);
+            this.btnCUDO.TabIndex = 7;
+            this.btnCUDO.Text = "...";
+            this.btnCUDO.UseVisualStyleBackColor = true;
+            this.btnCUDO.Click += new System.EventHandler(this.btnCUDO_Click);
+            // 
+            // btnCDOC
+            // 
+            this.btnCDOC.Location = new System.Drawing.Point(437, 429);
+            this.btnCDOC.Name = "btnCDOC";
+            this.btnCDOC.Size = new System.Drawing.Size(75, 23);
+            this.btnCDOC.TabIndex = 8;
+            this.btnCDOC.Text = "...";
+            this.btnCDOC.UseVisualStyleBackColor = true;
+            this.btnCDOC.Click += new System.EventHandler(this.btnCDOC_Click);
+            // 
+            // gridUDO
+            // 
+            this.gridUDO.BackgroundColor = System.Drawing.Color.White;
+            this.gridUDO.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridUDO.Location = new System.Drawing.Point(29, 101);
+            this.gridUDO.Name = "gridUDO";
+            this.gridUDO.RowHeadersWidth = 51;
+            this.gridUDO.RowTemplate.Height = 24;
+            this.gridUDO.Size = new System.Drawing.Size(1393, 302);
+            this.gridUDO.TabIndex = 9;
+            // 
+            // gridDOC
+            // 
+            this.gridDOC.BackgroundColor = System.Drawing.Color.White;
+            this.gridDOC.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridDOC.Location = new System.Drawing.Point(29, 475);
+            this.gridDOC.Name = "gridDOC";
+            this.gridDOC.RowHeadersWidth = 51;
+            this.gridDOC.RowTemplate.Height = 24;
+            this.gridDOC.Size = new System.Drawing.Size(1393, 302);
+            this.gridDOC.TabIndex = 10;
+            // 
+            // btnLimpDoc
+            // 
+            this.btnLimpDoc.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpDoc.Location = new System.Drawing.Point(1285, 422);
+            this.btnLimpDoc.Name = "btnLimpDoc";
+            this.btnLimpDoc.Size = new System.Drawing.Size(137, 34);
+            this.btnLimpDoc.TabIndex = 11;
+            this.btnLimpDoc.Text = "LIMPIAR";
+            this.btnLimpDoc.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CargaReembolso.Properties.Resources.LogoSS;
+            this.pictureBox1.Location = new System.Drawing.Point(1230, 785);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(158, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(1235, 833);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 15);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Copyright";
+            // 
+            // CargaReemForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1451, 857);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnLimpDoc);
+            this.Controls.Add(this.gridDOC);
+            this.Controls.Add(this.gridUDO);
+            this.Controls.Add(this.btnCDOC);
+            this.Controls.Add(this.btnCUDO);
+            this.Controls.Add(this.cmbUDOs);
+            this.Controls.Add(this.cmbDocs);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSimular);
+            this.Controls.Add(this.btnProcesar);
+            this.Controls.Add(this.btnLimpUDO);
+            this.Name = "CargaReemForm";
+            this.Text = "CARGA REEMBOLSOS";
+            ((System.ComponentModel.ISupportInitialize)(this.gridUDO)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridDOC)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLimpUDO;
+        private System.Windows.Forms.Button btnProcesar;
+        private System.Windows.Forms.Button btnSimular;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbDocs;
+        private System.Windows.Forms.ComboBox cmbUDOs;
+        private System.Windows.Forms.Button btnCUDO;
+        private System.Windows.Forms.Button btnCDOC;
+        private System.Windows.Forms.DataGridView gridUDO;
+        private System.Windows.Forms.DataGridView gridDOC;
+        private System.Windows.Forms.Button btnLimpDoc;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ CargaReembolso/CargaReemForm.cs
@@ -0,0 +1,423 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.IO;
+using ExcelDataReader;
+
+namespace CargaReembolso
+{
+    public partial class CargaReemForm : Form
+    {
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HTCAPTION = 0x2;
+
+        private readonly Color ColorPrimary = Color.FromArgb(44, 76, 96);   // Azul barra superior / botones
+        private readonly Color ColorPrimaryHover = Color.FromArgb(36, 62, 78);
+        private readonly Color ColorBorder = Color.FromArgb(44, 76, 96);    // Borde
+        private readonly Color ColorHeader = Color.FromArgb(224, 224, 224); // Encabezados grid
+        private readonly Color ColorText = Color.White;
+        private readonly int TopBarHeight = 30;
+        private Rectangle _topBarRect;
+        private Rectangle _closeButtonRect;
+        private bool _hoverClose = false;
+        public string[] strTest = new string[4];
+        public string strConnString = "";
+        public string sCookie;
+        public bool conectarse = true;
+        public int ret;
+        public string strSQL;
+
+        public SAPbouiCOM.Application rSboApp;
+        public SAPbouiCOM.SboGuiApi rSboGui;
+        public SAPbobsCOM.Company rCompany;
+
+        public String Anio = DateTime.Today.Year.ToString(); 
+
+        public CargaReemForm()
+        {
+            InitializeComponent();
+            UpdateRects();
+            ApplyTheme();
+            UpdateRects();
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Icon = Properties.Resources.LogoV2;
+            label3.Text = $"Copyright © {Anio} SOLSAP S.A.";
+
+            strTest = System.Environment.GetCommandLineArgs();
+
+            if (strTest.Length > 0)
+            {
+                if (strTest.Length > 1)
+                {
+                    if (strTest[0].LastIndexOf("\\") > 0)
+                    {
+                        strConnString = strTest[1];
+                    }
+                    else
+                    {
+                        strConnString = strTest[0];
+                    }
+                }
+                else
+                {
+                    if (strTest[0].LastIndexOf("\\") == -1)
+                    {
+                        strConnString = strTest[0];
+                    }
+                    else
+                    {
+                        MessageBox.Show("El Add-on se debe ejecutar desde SAP Business One. (" + "Carga Reembolso" + "-Err1)");
+                        Environment.Exit(0);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("El Add-on se debe ejecutar desde SAP Business One. (" + "Carga Reembolso" + "-Err2)");
+                Environment.Exit(0);
+            }
+
+
+            if (strConnString.Length > 0){
+                try
+                {
+                    rSboGui = new SAPbouiCOM.SboGuiApi();
+                    rCompany = new SAPbobsCOM.Company();
+
+                    // Conexión con el UI
+                    rSboGui.Connect(strConnString);
+                    rSboApp = rSboGui.GetApplication();
+
+                    if (conectarse)
+                    {
+                        rCompany = rSboApp.Company.GetDICompany();
+                    }
+                    else
+                    {
+                        sCookie = rCompany.GetContextCookie();
+                        ret = rCompany.SetSboLoginContext(rSboApp.Company.GetConnectionContext(sCookie));
+                        if(ret == 0)
+                        {
+                            ret = rCompany.Connect();
+                            if(ret != 0)
+                            {
+                                rCompany.GetLastError(out ret, out strSQL);
+                                rSboApp.StatusBar.SetText("Error al Conectar el Add-On " + "Carga Reembolso" + ": " + strSQL, SAPbouiCOM.BoMessageTime.bmt_Medium, SAPbouiCOM.BoStatusBarMessageType.smt_Error);
+                                Environment.Exit(0);
+
+                            }
+                        }
+                        else
+                        {
+                            rSboApp.StatusBar.SetText("No se ha Conectado con AddOn " + "Carga Reembolso" + ": Error " + ret, SAPbouiCOM.BoMessageTime.bmt_Short, SAPbouiCOM.BoStatusBarMessageType.smt_Error);
+                            Environment.Exit(0);
+                        }
+                    }
+                }
+                catch(Exception ex)
+                {
+
+                }
+            }
+
+        }
+
+        private void ApplyTheme()
+        {
+            // Fondo del form
+            this.BackColor = Color.White;
+
+            // --- Franja superior: reservamos área para pintar el "título" ---
+            _topBarRect = new Rectangle(0, 0, this.ClientSize.Width, TopBarHeight);
+            //this.Padding = new Padding(0, TopBarHeight, 0, 0); // baja el contenido
+
+            // --- Botones principales ---
+            StyleButton(btnProcesar);
+            StyleButton(btnSimular);
+            StyleButton(btnLimpUDO);
+            StyleButton(btnLimpDoc);
+
+            // Botones "..." (más pequeños, mismos colores)
+            StyleSmallButton(btnCUDO);
+            StyleSmallButton(btnCDOC);
+
+            // Labels
+            label1.ForeColor = Color.FromArgb(64, 64, 64);
+            label2.ForeColor = Color.FromArgb(64, 64, 64);
+
+            // Combos
+            StyleCombo(cmbUDOs);
+            StyleCombo(cmbDocs);
+
+            // Grids
+            StyleGrid(gridUDO);
+            StyleGrid(gridDOC);
+
+            // Redibujo
+            this.Invalidate();
+        }
+
+        private void StyleButton(Button b)
+        {
+            b.FlatStyle = FlatStyle.Flat;
+            b.FlatAppearance.BorderSize = 0;
+            b.BackColor = ColorPrimary;
+            b.ForeColor = ColorText;
+            b.Cursor = Cursors.Hand;
+            b.FlatAppearance.MouseOverBackColor = ColorPrimaryHover;
+            b.FlatAppearance.MouseDownBackColor = ColorPrimaryHover;
+        }
+
+        private void StyleSmallButton(Button b)
+        {
+            b.FlatStyle = FlatStyle.Flat;
+            b.FlatAppearance.BorderSize = 0;
+            b.BackColor = ColorPrimary;
+            b.ForeColor = ColorText;
+            b.Width = 36; // visual tipo "…"
+            b.Cursor = Cursors.Hand;
+            b.FlatAppearance.MouseOverBackColor = ColorPrimaryHover;
+            b.FlatAppearance.MouseDownBackColor = ColorPrimaryHover;
+        }
+
+        private void StyleCombo(ComboBox c)
+        {
+            c.DropDownStyle = ComboBoxStyle.DropDownList;
+            c.FlatStyle = FlatStyle.Standard;
+            c.BackColor = Color.White;
+            c.ForeColor = Color.FromArgb(40, 40, 40);
+        }
+
+        private void StyleGrid(DataGridView g)
+        {
+            //g.BackgroundColor = Color.White;
+            //g.BorderStyle = BorderStyle.None;
+            //g.EnableHeadersVisualStyles = false;
+
+            //g.ColumnHeadersDefaultCellStyle.BackColor = ColorHeader;
+            //g.ColumnHeadersDefaultCellStyle.ForeColor = Color.FromArgb(64, 64, 64);
+            //g.ColumnHeadersDefaultCellStyle.SelectionBackColor = ColorHeader;
+            //g.ColumnHeadersDefaultCellStyle.SelectionForeColor = Color.FromArgb(64, 64, 64);
+
+            //g.DefaultCellStyle.BackColor = Color.White;
+            //g.DefaultCellStyle.ForeColor = Color.FromArgb(40, 40, 40);
+            //g.DefaultCellStyle.SelectionBackColor = Color.FromArgb(235, 243, 249);
+            //g.DefaultCellStyle.SelectionForeColor = Color.FromArgb(32, 32, 32);
+            //g.GridColor = Color.FromArgb(230, 230, 230);
+
+            //g.RowHeadersVisible = false;
+            //g.ColumnHeadersHeight = 28;
+        }
+
+        // Pintamos barra superior + borde coloreado
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            // Barra superior
+            using (var b = new SolidBrush(ColorPrimary))
+                e.Graphics.FillRectangle(b, _topBarRect);
+
+            // Texto del título
+            using (var t = new SolidBrush(Color.White))
+            using (var f = new Font("Segoe UI", 9F, FontStyle.Bold))
+                e.Graphics.DrawString("CARGA DE REEMBOLSOS - " + rCompany.CompanyName.ToString().ToUpper(), f, t, 12, (_topBarRect.Height - f.Height) / 2);
+
+            // Botón cerrar (rectángulo)
+            int btnSize = 30;
+            _closeButtonRect = new Rectangle(this.ClientSize.Width - btnSize, 0, btnSize, TopBarHeight);
+
+            using (var b = new SolidBrush(_hoverClose ? Color.Red : ColorPrimary))
+                e.Graphics.FillRectangle(b, _closeButtonRect);
+
+            // Dibujar la "X"
+            using (var pen = new Pen(Color.White, 2))
+            {
+                int padding = 8;
+                e.Graphics.DrawLine(pen, _closeButtonRect.Left + padding, _closeButtonRect.Top + padding,
+                                          _closeButtonRect.Right - padding, _closeButtonRect.Bottom - padding);
+                e.Graphics.DrawLine(pen, _closeButtonRect.Right - padding, _closeButtonRect.Top + padding,
+                                          _closeButtonRect.Left + padding, _closeButtonRect.Bottom - padding);
+            }
+        }
+
+        // Redimensionar: mantener la barra superior al ancho
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            base.OnSizeChanged(e);
+            _topBarRect = new Rectangle(0, 0, this.ClientSize.Width, TopBarHeight);
+            UpdateRects();
+        }
+
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+            if (e.Button == MouseButtons.Left &&
+                _topBarRect.Contains(e.Location) &&
+                !_closeButtonRect.Contains(e.Location))   // << clave
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HTCAPTION, 0);
+            }
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+            if (_closeButtonRect.Contains(e.Location))
+            {
+                if (!_hoverClose)
+                {
+                    _hoverClose = true;
+                    this.Invalidate(_closeButtonRect);
+                }
+            }
+            else if (_hoverClose)
+            {
+                _hoverClose = false;
+                this.Invalidate(_closeButtonRect);
+            }
+        }
+
+        protected override void OnMouseClick(MouseEventArgs e)
+        {
+            base.OnMouseClick(e);
+            if (_closeButtonRect.Contains(e.Location) && e.Button == MouseButtons.Left)
+            {
+                Application.Exit();
+            }
+        }
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            string url = "https://solsaptech.com/"; // URL que deseas abrir
+            try
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = url,
+                    UseShellExecute = true  // Necesario para .NET Framework 4.8 y Windows 10/11
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se pudo abrir el enlace: " + ex.Message);
+            }
+        }
+
+        private void UpdateRects()
+        {
+            _topBarRect = new Rectangle(0, 0, this.ClientSize.Width, TopBarHeight);
+            int btnSize = 30;
+            _closeButtonRect = new Rectangle(this.ClientSize.Width - btnSize, 0, btnSize, TopBarHeight);
+        }
+
+        private void btnCUDO_Click(object sender, EventArgs e)
+        {
+            OpenExcelAndLoad(gridUDO);
+        }
+
+        private TabControl EnsureTabControlFor(DataGridView placeholderGrid)
+        {
+            var parent = placeholderGrid.Parent;
+            var tabName = placeholderGrid.Name + "Tabs";
+            var existing = parent.Controls.Find(tabName, false).FirstOrDefault() as TabControl;
+            if (existing != null) return existing;
+
+            var tabs = new TabControl
+            {
+                Name = tabName,
+                Bounds = placeholderGrid.Bounds,
+                Anchor = placeholderGrid.Anchor
+            };
+            parent.Controls.Add(tabs);
+            tabs.BringToFront();
+            placeholderGrid.Visible = false;   // ocultamos el grid “placeholder”
+            return tabs;
+        }
+
+        // Aplica tu estilo a cada DataGridView nuevo (reusa tu StyleGrid)
+        private void ApplyGridStyle(DataGridView g)
+        {
+            // Usa tu método existente para estilizar
+            StyleGrid(g); // ya lo tienes en tu form
+        }
+
+        // Carga un archivo Excel y crea una pestaña por cada hoja
+        private void LoadExcelToTabs(string excelPath, DataGridView placeholderGrid)
+        {
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
+            using (var stream = File.Open(excelPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (var reader = ExcelReaderFactory.CreateReader(stream))
+            {
+                var conf = new ExcelDataSetConfiguration
+                {
+                    ConfigureDataTable = _ => new ExcelDataTableConfiguration
+                    {
+                        UseHeaderRow = true
+                    }
+                };
+                var dataSet = reader.AsDataSet(conf);
+
+                var tabs = EnsureTabControlFor(placeholderGrid);
+                tabs.TabPages.Clear();
+
+                foreach (DataTable table in dataSet.Tables)
+                {
+                    var page = new TabPage
+                    {
+                        Text = string.IsNullOrWhiteSpace(table.TableName) ? "Hoja" : table.TableName
+                    };
+
+                    var grid = new DataGridView
+                    {
+                        Dock = DockStyle.Fill,
+                        ReadOnly = false,
+                        AllowUserToAddRows = false,
+                        DataSource = table,
+                        BackgroundColor = Color.White
+                    };
+
+                    ApplyGridStyle(grid);
+                    page.Controls.Add(grid);
+                    tabs.TabPages.Add(page);
+                }
+            }
+        }
+
+        // Diálogo de abrir Excel (solo .xlsx/.xls) y carga al grid destino
+        private void OpenExcelAndLoad(DataGridView targetGrid)
+        {
+            using (var ofd = new OpenFileDialog())
+            {
+                ofd.Title = "Seleccione archivo de Excel";
+                ofd.Filter = "Archivos de Excel|*.xlsx;*.xls";
+                ofd.Multiselect = false;
+                if (ofd.ShowDialog(this) == DialogResult.OK)
+                {
+                    LoadExcelToTabs(ofd.FileName, targetGrid);
+                }
+            }
+        }
+
+        private void btnCDOC_Click(object sender, EventArgs e)
+        {
+            OpenExcelAndLoad(gridDOC);
+        }
+    }
+}
